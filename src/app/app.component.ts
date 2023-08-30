f33f2a5839014d6a90406a969eab3e536421bb8d
c-sathish <csathishkumar217@gmail.com>
2023-08-30 15:13:09 +0530

    Initial angular code commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
+import { Observable } from 'rxjs';
+import { ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'sample-website-app';
+export class AppComponent  {
+
+  // Pie
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: false,
+  };
+  public pieChartLabels = [ 'Open','In-Progress','Completed' ];
+  public pieChartDatasets = [ {
+    data: [ 30, 50, 10 ]
+  } ];
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
+
+  // Each Column Definition results in one Column.
+  public columnDefs: ColDef[] = [
+    { field: 'make'},
+    { field: 'model'},
+    { field: 'price' }
+  ];
+
+  // DefaultColDef sets props common to all Columns
+  public defaultColDef: ColDef = {
+    sortable: true,
+    filter: true,
+  };
+  
+  // Data that gets displayed in the grid
+  public rowData$!: Observable<any[]>;
+
+  // For accessing the Grid's API
+  @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
+
+  constructor(private http: HttpClient) {}
+
+  // Example load data from server
+  onGridReady(params: GridReadyEvent) {
+    this.rowData$ = this.http
+      .get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+      params.api.sizeColumnsToFit();
+  }
+
+  // Example of consuming Grid Event
+  onCellClicked( e: CellClickedEvent): void {
+    console.log('cellClicked', e);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'
+import { AgGridModule } from 'ag-grid-angular';
 import { AppComponent } from './app.component';
-
+import { NgChartsModule } from 'ng2-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AgGridModule,
+    NgChartsModule,
+    BsDatepickerModule.forRoot(),
+    TabsModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
